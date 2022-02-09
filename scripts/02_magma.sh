3c07d12613710b2475d2dd91c6ed34cdea4f2cc1
frhl <frederik.heymann@gmail.com>
2022-02-09 10:53:15 +0000

    Magma now functional

--- scripts/02_magma.sh
+++ scripts/02_magma.sh
@@ -15,7 +15,7 @@ module load gcccuda/2020b
 readonly in_dir="data/sumstat/giant"
 readonly out_dir="data/magma"
 
-readonly sumstat="${in_dir}/PublicRelease.WHRadjBMI.C.Eur.Add.txt.gz"
+readonly sumstat="${in_dir}/PublicRelease.WHRadjBMI.C.Eur.Add.txt"
 readonly out_prefix="${out_dir}/WHRadjBMI_C_Eur_ADD"
 #readonly annot_prefix="${out_dir}/WHRadjBMI_C_Eur_ADD_annot"
 #readonly gene_prefix="${out_dir}/WHRadjBMI_C_Eur_ADD_gene"
@@ -27,10 +27,19 @@ readonly genes="${magma_dir}/auxiliary_files/genes/GRCh37/NCBI37.gene.loc"
 readonly dbsnp="${magma_dir}/auxiliary_files/dbsnp/dbsnp151.synonyms"
 readonly magma="${magma_dir}/./magma"
 
+# * Note: Only SNPs in reference panel and target are used for analysis and
+# we should therefore generate a new panel using all imputed SNPs with info > 0.8
+# * Note: MAGMA can not read in gzipped data
+
 mkdir -p ${out_dir}
 
-zcat ${sumstat} | awk '{print $1"\t"$2"\t"$3}' > ${snp_loc}
-
+# Format gene gene loc file
+if [ ! -f ${snp_loc} ]; then
+  cat ${sumstat} | awk '{print $1"\t"$2"\t"$3}' > ${snp_loc}
+else
+  >&2 echo "${snp_loc} already exists. Skipping.."
+fi
+# Annotation Step (SNP to gene mapping)
 if [ ! -f "${out_prefix}.genes.annot" ]; then
   set -x
   ${magma} \
@@ -44,14 +53,15 @@ else
 fi
 
 
-
+# Gene Analysis Step (calculate gene p-values + other gene-level metrics)
 ${magma} \
     --bfile ${prefix_ref} \
     --gene-annot "${out_prefix}.genes.annot" \
-    --pval ${sumstat} \
-    --out ${out_prefix} \
-    --ncol "n" \
+    --pval "${sumstat}" 'pval=9' 'snp-id=1' 'ncol=10' \
+    --out ${out_prefix} 
  
+#snp-id=snpname "pval=pvalue" "ncol=n" \
+ #snp-id="snoname" pval="pvalue"
 
 
 
