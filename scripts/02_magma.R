f99d980baf3e5d06a39346e89f2689a6b36e59f0
frhl <frederik.heymann@gmail.com>
2022-05-30 14:41:30 +0100

    checkpoint

--- scripts/02_magma.R
+++ scripts/02_magma.R
@@ -2,25 +2,26 @@
 library(argparse)
 library(data.table)
 
-main <- function(){
-
-  d <- fread(args$in_path)
+main <- function(args){
+ 
+  stopifnot(file.exists(args$in_file)) 
+  d <- fread(args$in_file)
   genes <- fread('/well/lindgren/flassen/software/magma/auxiliary_files/genes/GRCh38/NCBI38.gene.loc')
   gene_mapping <- data.table(GENE = genes$V1, HGNC_SYMBOL=genes$V6)
   mrg <- merge(d, gene_mapping, all.x = TRUE)
   mrg$FDR <- stats::p.adjust(mrg$P, method = 'fdr')
   mrg <- mrg[order(mrg$P),]
-  fwrite(mrg, file = args$outpath, sep = args$out_sep)
+  fwrite(mrg, file = args$out_file, sep = args$out_sep)
 
 }
 
 
 # add arguments
 parser <- ArgumentParser()
-parser$add_argument("--in_path", default=NULL, required = TRUE, help = "Input")
-parser$add_argument("--out_path", default=NULL, required = TRUE, help = "Output")
+parser$add_argument("--in_file", default=NULL, required = TRUE, help = "Input")
+parser$add_argument("--out_file", default=NULL, required = TRUE, help = "Output")
 parser$add_argument("--out_sep", default="\t", help = "Output seperator")
 args <- parser$parse_args()
 
-main()
+main(args)
 
--- scripts/02_magma.sh
+++ scripts/02_magma.sh
@@ -15,19 +15,24 @@ module load gcccuda/2020b
 source utils/bash_utils.sh
 
 readonly in_dir="data/sumstat/giant/exome"
-readonly out_dir="data/magma/WHRadjBMI_eur_add"
+readonly out_dir="data/magma/WHRadjBMI_eur_add_ref10k"
 
 readonly sumstat="${in_dir}/PublicRelease.WHRadjBMI.C.Eur.Add.txt"
 readonly out_prefix="${out_dir}/WHRadjBMI_C_Eur_ADD"
 readonly snp_loc="${out_prefix}.snp_loc"
 
 readonly magma_dir="/well/lindgren/flassen/software/magma"
-readonly prefix_ref="${magma_dir}/auxiliary_files/reference/g1000_eur"
+#readonly prefix_ref="${magma_dir}/auxiliary_files/reference/g1000_eur"
+readonly ref_dir="/well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb/data/prs/hapmap"
+readonly prefix_ref="${ref_dir}/long_ukb_hapmap_rand_10k_eur"
 readonly genes="${magma_dir}/auxiliary_files/genes/GRCh37/NCBI37.gene.loc"
 readonly dbsnp="${magma_dir}/auxiliary_files/dbsnp/dbsnp151.synonyms"
 readonly magma="${magma_dir}/./magma"
 
-readonly rscript=
+# setup MAGMA window
+readonly window="" #"window=50,50"
+
+readonly rscript="scripts/02_magma.R"
 
 # * Note: Only SNPs in reference panel and target are used for analysis and
 # we should therefore generate a new panel using all imputed SNPs with info > 0.8
@@ -39,27 +44,31 @@ mkdir -p ${out_dir}
 cat ${sumstat} | awk '{print $1"\t"$2"\t"$3}' > ${snp_loc}
 
 # Annotation Step (SNP to gene mapping)
+set -x
 ${magma} \
-  --annotate \
+  --annotate "${window}"\
   --snp-loc "${snp_loc}" \
   --gene-loc "${genes}" \
   --out "${out_prefix}" 
+set +x
 
 # Gene Analysis Step (calculate gene p-values + other gene-level metrics)
+set -x
 ${magma} \
   --bfile "${prefix_ref}" \
   --gene-annot "${out_prefix}.genes.annot" \
   --pval "${sumstat}" "pval=9" "snp-id=1" "ncol=10" \
   --out "${out_prefix}"
+set +x
 
-# move magma log
-mv "magma.log" "${out_dir}/magma.log"
 
-# clean up files
 set_up_rpy
+set -x
 Rscript ${rscript} \
-  --in_path "${out_prefix}.genes.out" \
-  --out_path "${out_prefix}.txt" \
-  --out_sep "\n"
+  --in_file "${out_prefix}.genes.out" \
+  --out_file "${out_prefix}.txt" \
+  --out_sep "\t"
+set +x
+
 
 
